0a6d4304f8b7c07c2c6102aebac29fc0edd8b947
chrisrestrick1 <chrisrestrick1@googlemail.com>
2016-02-15 15:35:35 +0000

    IP3
    Files

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+session_start()
+?>
+<html>
+<title>Starter</title>
+<head>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.0/jquery.min.js"></script>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/jquery-ui.min.js"></script>
+</head>
+<body>
+<h1>Lets get started</h1>
+<style>
+  #draggable { width: 200px; height: 40px; padding: 0.5em; background-color:lightgrey ; }
+  
+  </style>
+  <script>
+  $(function() {
+    $( "#draggable" ).draggable();
+  });
+  </script>
+  <div id="draggable"></div>
+  <div id="draggable"></div>
+</body>
+</html>
